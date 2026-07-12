3bf7ed609f4a74ca5b257fd227174cdf61d5fe78
Diana Muzhylivska <diana.listen@gmail.com>
2026-07-12 12:31:57 +0300

    final

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -9,40 +9,42 @@ interface SearchBarProps {
 export default function SearchBar({ onSubmit }: SearchBarProps) {
   const handleSubmit = (formData: FormData) => {
     const query = formData.get("query") as string;
-    
-    // Перевірка на порожній рядок (вимагає ТЗ)
+
+    // Перевірка на порожній рядок
     if (!query || query.trim() === "") {
       toast.error("Please enter your search query.");
       return;
-      }
-      
+    }
+
     // Передаємо чисте значення без зайвих пробілів на початку/в кінці
     onSubmit(query.trim());
   };
 
   return (
     <header className={styles.header}>
-        <div className={styles.container}>
-            <a className={styles.link}
-                href="https://www.themoviedb.org/"
-                target="_blank"
-                rel="noopener noreferrer">
-                  Powered by TMDB
-            </a>
-            <form action={handleSubmit} className={styles.form}>
-                <input
-                    className={styles.input}
-                    type="text"
-                    name="query"
-                    autoComplete="off"
-                    placeholder="Search movies..."
-                    autoFocus
-                />
-                <button className={styles.button} type="submit">
-                  Search
-                </button>
-            </form>
-        </div>
+      <div className={styles.container}>
+        <a
+          className={styles.link}
+          href="https://www.themoviedb.org/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by TMDB
+        </a>
+        <form action={handleSubmit} className={styles.form}>
+          <input
+            className={styles.input}
+            type="text"
+            name="query"
+            autoComplete="off"
+            placeholder="Search movies..."
+            autoFocus
+          />
+          <button className={styles.button} type="submit">
+            Search
+          </button>
+        </form>
+      </div>
     </header>
   );
 }
